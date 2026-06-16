5ff3b0b52ea42cadcd8c8e1de55d062b9a9c2dee
davestinhast <ggabrielandre245@gmail.com>
2026-06-16 14:25:53 -0500

    TRABAJO S9

--- api-backend/src/app/prisma.service.ts
+++ api-backend/src/app/prisma.service.ts
@@ -1,11 +1,14 @@
-// api-backend/src/app/prisma.service.ts
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
-// @Injectable() permite que NestJS inyecte esta clase donde sea necesaria
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
-  // onModuleInit() se ejecuta automáticamente cuando NestJS inicia el módulo
+  constructor() {
+    super({
+      datasourceUrl: process.env.DATABASE_URL,
+    });
+  }
+
   async onModuleInit() {
     await this.$connect();
     console.log('📦 Conexión a Sentinel DB (PostgreSQL) establecida correctamente.');
